77491e7610a058b9aef14645315744f5fa52b55b
Will Luft <william.d.luft@gmail.com>
2021-01-07 20:49:08 -0500

    Added database connection

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ dotenv.config();
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-regular-89104',
-    user: 'will luft',
-    password: '',
-    database: 'smart-brain',
+    host: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
