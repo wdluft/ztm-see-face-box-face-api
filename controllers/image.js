135f22496797347494065883ee57c03eac6f53c7
Will Luft <william.d.luft@gmail.com>
2021-01-09 10:31:38 -0500

    Cleaned up code with prettier/linter

--- controllers/image.js
+++ controllers/image.js
@@ -8,25 +8,26 @@ const app = new Clarifai.App({
 });
 
 const handleAPICall = (req, res) => {
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-  .then(data => {
-    res.json(data)
-  })
-  .catch(err => res.status(400).json('unable to work with api'));
-}
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with api'));
+};
 
 const handleImage = (req, res, db) => {
-  const {id} = req.body;
-  
-  db('users').where('id', '=', id)
+  const { id } = req.body;
+
+  db('users')
+    .where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
-    .then(entries => {
-      res.json(entries[0])
+    .then((entries) => {
+      res.json(entries[0]);
     })
-    .catch(err => res.status(400).json('unable to get entries'));
-
-}
+    .catch((err) => res.status(400).json('unable to get entries'));
+};
 
 module.exports = {
   handleImage,
