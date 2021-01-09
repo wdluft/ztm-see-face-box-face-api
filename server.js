71da43579c5639b84d7384287bdb5470a6e88fe5
Will Luft <william.d.luft@gmail.com>
2021-01-09 10:09:38 -0500

    Added form validation to register route

--- server.js
+++ server.js
@@ -12,16 +12,15 @@ const image = require('./controllers/image');
 
 dotenv.config();
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-  }
+  },
 });
-
 
 const app = express();
 
@@ -29,25 +28,46 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Routes 
+// Routes
 app.get('/', (req, res) => {
   res.send('api server is working');
 });
 
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 
-app.post('/register', [check('email').normalizeEmail().isEmail().isEmpty(), check('name').isEmpty(), check('password', 'The password must be 8+ characters long and contain a number')
-.not()
-.isEmpty()
-.isIn(['123', 'password', 'god'])
-.withMessage('Do not use a common word as the password')
-.isLength({ min: 8 })
-.matches(/\d/)], (req, res) => {register.handleRegister(req, res, db, bcrypt, validationResult)});
+app.post(
+  '/register',
+  [
+    check('name').not().isEmpty().withMessage('Name cannot be blank'),
+    check('email')
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Must use a valid email')
+      .not()
+      .isEmpty()
+      .withMessage('Email cannot be blank'),
 
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+    check('password')
+      .isLength({ min: 8 })
+      .withMessage('Password must be 8+ characters long')
+      .matches(/\d/)
+      .withMessage('Password must contain a number'),
+  ],
+  (req, res) => {
+    register.handleRegister(req, res, db, bcrypt, validationResult);
+  }
+);
 
-app.put('/image', (req, res) => {image.handleImage(req, res, db)} );
-app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleAPICall(req, res);
+});
 
 app.listen(process.env.PORT || 6969, () => {
   console.log(`🚀🚀🚀 App is runnin on port ${process.env.PORT} 🔥🔥🔥`);
