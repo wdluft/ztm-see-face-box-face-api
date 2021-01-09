488150ee47f39df7995f006504e3a789cd1f4f79
Will Luft <william.d.luft@gmail.com>
2021-01-08 21:01:10 -0500

    Adding register form validation

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
+const { check, validationResult } = require('express-validator');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -35,13 +36,19 @@ app.get('/', (req, res) => {
 
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+app.post('/register', [check('email').normalizeEmail().isEmail().isEmpty(), check('name').isEmpty(), check('password', 'The password must be 8+ characters long and contain a number')
+.not()
+.isEmpty()
+.isIn(['123', 'password', 'god'])
+.withMessage('Do not use a common word as the password')
+.isLength({ min: 8 })
+.matches(/\d/)], (req, res) => {register.handleRegister(req, res, db, bcrypt, validationResult)});
 
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 
 app.put('/image', (req, res) => {image.handleImage(req, res, db)} );
 app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)});
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 6969, () => {
   console.log(`🚀🚀🚀 App is runnin on port ${process.env.PORT} 🔥🔥🔥`);
 });
